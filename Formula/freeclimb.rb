5ed6e8d1b5ecd049f7d9893af04e84e70c29f89d
Joseph Black <jblack@vailsys.com>
2021-04-06 14:49:11 -0500

    Add an initial formula file for the CLI

---
+++ Formula/freeclimb.rb
@@ -0,0 +1,26 @@
+require "language/node"
+
+class Freeclimb < Formula
+  desc "Install FreeClimb's CLI to start managing apps, buying numbers, and testing applications from your command line."
+  homepage "https://github.com/FreeClimbAPI/freeclimb-cli"
+  url "https://vail-freeclimb-cli.s3.us-east-2.amazonaws.com/freeclimb-v0.1.12/freeclimb-v0.1.12.tar.gz"
+  version "0.1.12"
+  sha256 ""
+  depends_on "node"
+
+  def install
+    libexec.install Dir["*"]
+    bin.install_symlink libexec/"bin/freeclimb"
+  end
+
+  def post_install
+    ohai "************************************************"
+    ohai "*                                              *"
+    ohai "* The FreeClimb CLI installation has finished. *"
+    ohai "*                                              *"
+    ohai "* To read the help resources, you can run:     *"
+    ohai "*       freeclimb --help                       *"
+    ohai "*                                              *"
+    ohai "************************************************"
+  end
+end
