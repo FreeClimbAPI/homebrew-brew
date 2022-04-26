5794403dd574fe602a64f4df0a51906342ae0b3e
Catherine Samatas <csamatas@vailsys.com>
2022-04-26 16:01:05 -0500

    manual update

--- Formula/freeclimb.rb
+++ Formula/freeclimb.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Freeclimb < Formula
   desc "Install FreeClimb's CLI to start managing apps, buying numbers, and testing applications from your command line."
   homepage "https://github.com/FreeClimbAPI/freeclimb-cli"
-  url "https://vail-freeclimb-cli.s3.us-east-2.amazonaws.com/freeclimb-v0.4.1/freeclimb-v0.4.1.tar.gz"
-  version "0.4.1"
+  url "https://vail-freeclimb-cli.s3.us-east-2.amazonaws.com/freeclimb-v0.5.3/freeclimb-v0.5.3.tar.gz"
+  version "0.5.3"
   sha256 "7daff25ec47847eaaa3a71f8e07bab3cc4946ef6153216a93ddb7a1aaf73460a"
   depends_on "node"
 
